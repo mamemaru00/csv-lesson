a4ff1d878abd5ef84c9bd0766cc8083eaa9f246b
mamemaru00 <k.shouhei0711@gmail.com>
2022-10-07 15:04:03 +0900

    ルート設定

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+Route::get('/', 'App\Http\Controllers\AddressController@index');
+Route::get('/export', 'App\Http\Controllers\AddressController@csvDownload')->name('export');
